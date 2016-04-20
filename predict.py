d102baa87fd950c2da0a43a94c715f1a68a910f5
Vinay Gaba <vinaygaba@gmail.com>
2016-04-20 14:47:00 -0400

    Added creation of map to store column number and types/categories

--- predict.py
+++ predict.py
@@ -1,17 +1,30 @@
 import numpy as np
 import pandas as pd
+import re
 from sklearn import tree
 
 input_file = "data/data.csv"
 
+field_types_file = "data/field_types.txt"
+D = {}
+
+with open(field_types_file, "r") as content:
+    array = []
+    for line in content:
+        temp_array = line.split()
+        category_array = temp_array[1:len(temp_array)]
+        D[temp_array[0]] = category_array
+        print temp_array[0]
+        print D[temp_array[0]]
 # comma delimited is the default
+
 df = pd.read_csv(input_file, header = 0)
 
-print df.dtypes;
+#print df.dtypes;
 
 
 # remove the non-numeric columns
-df = df._get_numeric_data()
+#df = df._get_numeric_data()
 
 # put the numeric column names in a python list
 numeric_headers = list(df.columns.values)
